e28117d3ad490722e5c6581eee60a1b42955ec5f
Valentyn Khalin <vkhalin@mirantis.com>
2016-12-22 16:30:59 +0530

    add correct script

--- mk-verificator/networking/mtu_checker.py
+++ mk-verificator/networking/mtu_checker.py
@@ -1,64 +1,204 @@
 #!/usr/bin/env python
+# RUN AS SUDO ON SALT MASTER
+# TODO: optionally use args to have a list of MTU for specific node set
+# TODO: print only FAILED MTU nodes
+
 import salt.client as client
 import texttable as tt
 
-local = client.LocalClient()
 
-pkgs_info = local.cmd('*', 'service.get_all')
-nodes = pkgs_info.keys()
+expectations = {
+    "ceph": {
+        "bond0": "9100",
+        "bond0.1201": "9100",
+        "bond0.1204": "9100",
+        "bond0.1208": "9100",
+        "cp": "9100",
+        "eno1": "9000",
+        "eno2": "9000",
+        "eno3": "9000",
+        "eno4": "9000",
+        "storage":"9100"
+    },
+    "kvm": {
+        "bond0": "9100",
+        "bond0.1201": "9100",
+        "bond0.1202": "9100",
+        "bond0.1203": "9100",
+        "bond0.1204": "9100",
+        "bond0.1208": "9100",
+        "br-cp": "9000",
+        "br-private": "9000",
+        "br-public": "9100",
+        "br-pxe": "9000",
+        "br-storage": "9100",
+        "eno1": "9000",
+        "eno2": "9000",
+        "eno3": "9100",
+        "eno4": "9100",
+        "vnet0": "9000",
+        "vnet1": "9000",
+        "vnet2": "9000",
+        "vnet3": "9000"
+    },
+    "prx":{
+        "eth0": "1500",
+        "eth1": "9000",
+        "eth2": "9000"
+    },
+    "cpu":{
+        "bond0": "9100",
+        "bond0.1202": "9100",
+        "bond0.1204": "9100",
+        "bond0.1208": "9100",
+        "bonding_masters": "9000",
+        "eno1": "1500",
+        "eno2": "1500",
+        "eno3": "9100",
+        "eno4": "9100",
+        "pkt0": "1500",
+        "pkt1": "65535",
+        "pkt2": "65535",
+        "pkt3": "65535",
+        "tap09bdf181-45": "9000",
+        "tap8a299d2d-5d": "9000",
+        "vhost0": "9000"
+    },
+    "rmq":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "sql":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "ctl":{
+        "eth0": "1500",
+        "eth1": "9000",
+        "eth2": "9000",
+        "eth3": "9000"
+    },
+    "des":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "nal":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "saml":{
+        "eth0": "1500",
+        "eth1": "9000",
+        "eth2": "9000"
+    },
+    "asc":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "ntw":{
+        "eth0": "1500",
+        "eth1": "9000",
+        "eth2": "9000"
+    },
+    "apt":{
+        "eth0": "1500"
+    },
+    "log":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "mtr":{
+        "eth0": "1500",
+        "eth1": "9000"
+    },
+    "mon":{
+        "eth0": "1500",
+        "eth1": "9000"
+    }
+}
+
+TOTAL = {}
+network_info = {}
+node_ifaces = {}
 
 
-for node_name, node_services in pkgs_info.items():
-    group_name = node_name.split('-')[0]
-    if not groups.has_key(group_name):
-        groups[group_name] = []
-    else:
-        groups[group_name].append((node_name, node_services))
+def get_node_ifaces(node_, node_ifaces_, local):
+    skipped_ifaces = ["bonding_masters", "lo"]
+    print "Trying to gather ifaces info for {}...".format(node_)
+    ifaces = {}
+    for iface in node_ifaces_:
+        if iface in skipped_ifaces:
+            continue
+        try:
+            iface_mtu = local.cmd(node_, 'cmd.run',
+                                  ['cat /sys/class/net/{}/mtu'.format(iface)])
+            ifaces[iface] = iface_mtu.get(node_)
+        except Exception as e_:
+            print e_
+    TOTAL[node_] = ifaces
 
-services_to_skip = []
 
-
-def draw_table_missed_services(node_1_name, node_2_name, services_data):
+def draw_results_table(total):
+    print "Trying to draw a table with the results..."
     tab = tt.Texttable()
-    header = ['Service name']
-    tab.set_cols_align(['r'])
-    tab.set_cols_width([40])
-    tab.header(header)
-    for service_name in services_data:
-        tab.add_row([service_name])
-    s = tab.draw()
-    print "Service installed on %s, but not installed on %s" % (node_1_name, node_2_name)
-    print s
-    print 
+    tab.set_chars(['-', '|', '+', '-'])
+    tab.set_cols_align(["c", "c", "c", "c", "c"])
+    tab.set_cols_valign(["c", "c", "c", "c", "c"])
+    tab.add_row(["Node", "Iface", "MTU set", "MTU expected", "Result"])
 
-
-for group_name, nodes in groups.items():
-    print "-" * 140
-    if len(nodes) > 1:
-
-        print "Start verification for group %s" % group_name
-
-        for node_i in nodes:
-            for node_j in nodes:
-                if node_i[0] == node_j[0]:
+    for node_ in total:
+            ifaces = total.get(node_)
+            node_name_ = node_.split('-')[0]
+            tab.add_row([node_, "", "", "", ""])
+            for iface in ifaces:
+                if node_name_ not in expectations:
                     continue
+                else:
+                    group = expectations.get(node_name_)
+                    gauge = group.get(iface)
+                    mtu = ifaces.get(iface)
+                    if iface not in group:
+                        tab.add_row(["", iface, mtu, "", ""])
+                    elif int(mtu) == int(gauge):
+                        tab.add_row(["", iface, mtu, gauge, "+"])
+                    else:
+                        tab.add_row(["", iface, mtu, gauge, "FAILED"])
 
-                node_i_name, node_j_name = node_i[0], node_j[0]
-                node_i_services, node_j_services = node_i[1], node_j[1]
+    table = tab.draw()
 
-                missed_services = []
+    with open("table", "w") as file_:
+        file_.write(table)
+    print "----------Results gathered!----------"
+    print tab.draw()
 
-                for service_name in node_i_services:
-                    if service_name in services_to_skip:
-                        continue
 
-                    if service_name not in node_j_services:
-                        missed_services.append((service_name))
+def main():
+    global network_info, node_ifaces
+    local = client.LocalClient()
 
-                if missed_services:
-                    draw_table_missed_services(node_i_name, node_j_name, missed_services)
-                    print "-" * 140
+    try:
+        print "Trying to obtain nodes with interfaces..."
+        network_info = local.cmd('*', 'cmd.run', ['sudo ls /sys/class/net/'])
+    except Exception as e:
+        print e
 
-    else:
-        print "Verification for group %s was skipped due to count of nodes less than 2" % group_name
-    print "-" * 140
+    for node, ifaces_info in network_info.iteritems():
+        try:
+            node_name = node.split('-')[0]
+            node_ifaces = ifaces_info.split('\n')
+            if not expectations.has_key(node_name):
+                print "Node {} is not matching expected groups!".format(node)
+            else:
+                try:
+                    get_node_ifaces(node, node_ifaces, local)
+                except Exception as e:
+                    print e
+        except Exception as e:
+            print e
+
+    draw_results_table(TOTAL)
+    print "\nDONE"
+
+if __name__ == "__main__":
+    main()
+
