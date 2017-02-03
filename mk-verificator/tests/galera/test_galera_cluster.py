8c4251c7ec6177c7bcd6ff7334f05b5486ae11e1
dkulykovmirantis <den_0987654321@mail.ru>
2017-02-03 11:20:01 +0200

    Update test_galera_cluster.py

--- mk-verificator/tests/galera/test_galera_cluster.py
+++ mk-verificator/tests/galera/test_galera_cluster.py
@@ -1,1 +1,12 @@
-__author__ = 'msenin'
+def test_galera_cluster_status(local_salt_client):
+    gs = local_salt_client.cmd(
+        'sql-*',
+        'cmd.run',
+        ['salt-call mysql.status | grep -A1 wsrep_cluster_size | tail -n1'])
+    
+    size_cluster = []
+
+    for item in  gs.values():
+        size_cluster.append(item.strip())
+
+    assert size_cluster == ['3','3','3']
