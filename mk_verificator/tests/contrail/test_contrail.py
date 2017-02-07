c1d813613fcc231679d1c29dafe2b106a4bb324d
Oleksii Zhurba <legan4ik@gmail.com>
2017-02-08 03:32:32 +0530

    Change and improve contrail-status checks

--- mk_verificator/tests/contrail/test_contrail.py
+++ mk_verificator/tests/contrail/test_contrail.py
@@ -1,4 +1,4 @@
-def test_contrail_status(local_salt_client):
+def test_contrail_compute_status(local_salt_client):
     cs = local_salt_client.cmd(
         'cpu*', 'cmd.run', ['contrail-status | grep -Pv \'(==|^$)\'']
     )
@@ -15,3 +15,19 @@ def test_contrail_status(local_salt_client):
 
     assert not broken_services, 'Broken services: {}'.format(broken_services)
 
+def test_contrail_node_status(local_salt_client):
+    cs = local_salt_client.cmd(
+        'n[tw|al]*', 'cmd.run', ['contrail-status | grep -Pv \'(==|^$|Disk|unix|support)\'']
+    )
+    broken_services = []
+
+    for node in cs:
+        for line in cs[node].split('\n'):
+            line = line.strip()
+            name, status = line.split(None, 1)
+            if status not in {'active', 'backup'}:
+                err_msg = "{node}:{service} - {status}".format(
+                    node=node, service=name, status=status)
+                broken_services.append(err_msg)
+
+    assert not broken_services, 'Broken services: {}'.format(broken_services)
