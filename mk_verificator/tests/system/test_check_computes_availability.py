da71dba506b894ced285488030d6a7ed7c5edd42
dkulykovmirantis <den_0987654321@mail.ru>
2017-02-07 15:53:31 +0200

    Create test_check_computes_availability.py

---
+++ mk_verificator/tests/system/test_check_computes_availability.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_checking_nova_compute(local_salt_client):
+    service_info = local_salt_client.cmd(
+        'cpu*', 
+        'cmd.run', 
+       ['service nova-compute status'])
+
+    computes_no_availability = []
+
+    for node in service_info:
+        if not 'nova-compute start/running,' in service_info[node]:
+            computes_no_availability.append(node)
+
+    assert not computes_no_availability, \
+        "Nodes where nova-compute isn't started: {}".format(computes_no_availability)
