5d8465c68927fd1abdd24b7c4762debbbaea4940
legan4ik <legan4ik@gmail.com>
2017-02-09 02:36:34 -0600

    Adding state, ceph and contrail-status checks (#18)
    * Add test for checking nodes state
    
    * Change and improve contrail-status checks
    
    * Add primitive ceph osd check
    
    * Fix all flake8 problems

--- mk_verificator/tests/contrail/test_contrail.py
+++ mk_verificator/tests/contrail/test_contrail.py
@@ -1,4 +1,4 @@
-def test_contrail_status(local_salt_client):
+def test_contrail_compute_status(local_salt_client):
     cs = local_salt_client.cmd(
         'cpu*', 'cmd.run', ['contrail-status | grep -Pv \'(==|^$)\'']
     )
@@ -9,6 +9,25 @@ def test_contrail_status(local_salt_client):
             line = line.strip()
             name, status = line.split(None, 1)
             if status not in {'active'}:
+                err_msg = "{node}:{service} - {status}".format(
+                    node=node, service=name, status=status)
+                broken_services.append(err_msg)
+
+    assert not broken_services, 'Broken services: {}'.format(broken_services)
+
+
+def test_contrail_node_status(local_salt_client):
+    cs = local_salt_client.cmd(
+        'n[tw|al]*', 'cmd.run',
+        ['contrail-status | grep -Pv \'(==|^$|Disk|unix|support)\'']
+    )
+    broken_services = []
+
+    for node in cs:
+        for line in cs[node].split('\n'):
+            line = line.strip()
+            name, status = line.split(None, 1)
+            if status not in {'active', 'backup'}:
                 err_msg = "{node}:{service} - {status}".format(
                     node=node, service=name, status=status)
                 broken_services.append(err_msg)
