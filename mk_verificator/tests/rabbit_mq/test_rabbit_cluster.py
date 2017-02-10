e347883e89179301e6b2b761ed3848c20173aad0
Pavlo Ivanets <pivanets@mirantis.com>
2017-02-10 12:54:15 +0200

    Refactored code

--- mk_verificator/tests/rabbit_mq/test_rabbit_cluster.py
+++ mk_verificator/tests/rabbit_mq/test_rabbit_cluster.py
@@ -15,20 +15,18 @@ def test_checking_rabbitmq_cluster(local_salt_client):
     [required_cluster_size_dict.update(
         {node: len(cs.keys())}) for node in cs.keys()]
 
-    # create dictionary {node:cluster_size_taken_from_the_node}
-    actual_cluster_size_dict = {}
+    # in case if node cluster size differs from the required one, control dictionary
+    # is updated with {node:cluster_size_from_the_node}
+    control_dict = {}
     for node in cs:
         data = json.loads(cs[node])
-        actual_cluster_size_dict[node] =\
-            len(data['local']['rabbitmq:cluster']['members'])
 
-    d1 = required_cluster_size_dict
-    d2 = actual_cluster_size_dict
-    areEqual = sorted(d1.values()) == sorted(d2.values())\
-               and sorted(d1.keys()) == sorted(d2.keys())
+        cluster_size_from_the_node = len(data['local']['rabbitmq:cluster']['members'])
+        if required_cluster_size_dict[node] != cluster_size_from_the_node:
+            control_dict.update({node:cluster_size_from_the_node})
 
-    assert areEqual, \
+    assert not len(control_dict), \
         '''Inconsistency found within cloud. RabbitMQ cluster
-              is probably broken, the cluster size gathered from nodes is:
-              {} but should be {}'''.format(actual_cluster_size_dict,
-                                            required_cluster_size_dict)
+              is probably broken, the cluster size for each node should be:
+              {} but the following nodes has other values: {}'''.format(len(cs.keys()),
+                                                                        control_dict)
