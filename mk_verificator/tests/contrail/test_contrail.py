c23c4b38fd8d14508070b3fcb712b74cbf9cc8d6
Pavlo Ivanets <pivanets@mirantis.com>
2017-02-10 18:02:00 +0200

    Added test to check actual vRouter count and compare it with quantity of compute nodes

--- mk_verificator/tests/contrail/test_contrail.py
+++ mk_verificator/tests/contrail/test_contrail.py
@@ -1,3 +1,5 @@
+
+
 def test_contrail_compute_status(local_salt_client):
     cs = local_salt_client.cmd(
         'cpu*', 'cmd.run', ['contrail-status | grep -Pv \'(==|^$)\'']
@@ -14,7 +16,6 @@ def test_contrail_compute_status(local_salt_client):
                 broken_services.append(err_msg)
 
     assert not broken_services, 'Broken services: {}'.format(broken_services)
-
 
 def test_contrail_node_status(local_salt_client):
     cs = local_salt_client.cmd(
@@ -33,3 +34,19 @@ def test_contrail_node_status(local_salt_client):
                 broken_services.append(err_msg)
 
     assert not broken_services, 'Broken services: {}'.format(broken_services)
+
+def test_contrail_vrouter_count(local_salt_client):
+    cs = local_salt_client.cmd(
+        'cpu*', 'cmd.run', ['contrail-status | grep -Pv \'(==|^$)\'']
+    )
+
+    actual_vrouter_count = 0
+    for node in cs:
+        for line in cs[node].split('\n'):
+            if 'contrail-vrouter-nodemgr' in line:
+                actual_vrouter_count += 1
+
+    assert actual_vrouter_count == len(cs.keys()),\
+        'The length of vRouters {} differs' \
+        ' from the length of compute nodes {}'.format(actual_vrouter_count,
+                                                      len(cs.keys()))
