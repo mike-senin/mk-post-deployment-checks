817d187be16d4c0fe19fcab387eaed7b18f685c1
legan4ik <legan4ik@gmail.com>
2017-02-10 13:29:56 -0600

    Improve packet_checker (#23)
    * Improve packet_checker
    
    * Fix packet_checker filename and add todo for ceph test

--- mk_verificator/tests/ceph/test_ceph.py
+++ mk_verificator/tests/ceph/test_ceph.py
@@ -1,4 +1,5 @@
 def test_check_ceph_osd(local_salt_client):
+    # TODO: remove hardcode for salt run (ceph node mask)
     osd_fail_count = \
         local_salt_client.cmd('ceph-001*', 'cmd.run',
                               ['ceph osd tree | grep down | wc -l'])
---
+++ mk_verificator/tests/system/test_packet_checker.py
@@ -0,0 +1,73 @@
+import pytest
+import json
+from mk_verificator import utils
+
+
+@pytest.mark.parametrize(
+    ("group"),
+    utils.get_groups(utils.get_configuration(__file__))
+)
+def test_check_package_versions(local_salt_client, group):
+    output = local_salt_client.cmd(group, 'lowpkg.list_pkgs')
+
+    if len(output.keys()) < 2:
+        pytest.skip("Nothing to compare - only 1 node")
+
+    nodes = []
+    pkts_data = []
+    my_set = set()
+
+    for node in output:
+        nodes.append(node)
+        my_set.update(output[node].keys())
+
+    for deb in my_set:
+        row = []
+        row.append(deb)
+        for node in nodes:
+            if deb in output[node].keys():
+                row.append(output[node][deb])
+            else:
+                row.append("No package")
+        if row.count(row[1]) < len(nodes):
+            pkts_data.append(row)
+    assert len(pkts_data) <= 1, \
+        "Several problems found for {0} group: {1}".format(
+        group, json.dumps(pkts_data, indent=4))
+
+
+@pytest.mark.parametrize(
+    ("group"),
+    utils.get_groups(utils.get_configuration(__file__))
+)
+def test_check_module_versions(local_salt_client, group):
+    pre_check = local_salt_client.cmd(group, 'cmd.run',
+                                      ['dpkg -l | grep "python-pip "'])
+    if pre_check.values().count('') > 0:
+        pytest.skip("pip is not installed on one or more nodes")
+    if len(pre_check.keys()) < 2:
+        pytest.skip("Nothing to compare - only 1 node")
+    output = local_salt_client.cmd(group, 'pip.freeze')
+
+    nodes = []
+    pkts_data = []
+    my_set = set()
+
+    for node in output:
+        nodes.append(node)
+        my_set.update([x.split("=")[0] for x in output[node]])
+        output[node] = dict([x.split("==") for x in output[node]])
+
+    for deb in my_set:
+        row = []
+        row.append(deb)
+        for node in nodes:
+            if deb in output[node].keys():
+                row.append(output[node][deb])
+            else:
+                row.append("No module")
+        if row.count(row[1]) < len(nodes):
+            pkts_data.append(row)
+    assert len(pkts_data) <= 1, \
+        "Several problems found for {0} group: {1}".format(
+        group, json.dumps(pkts_data, indent=4))
