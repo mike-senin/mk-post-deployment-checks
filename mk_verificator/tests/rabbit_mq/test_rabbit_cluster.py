fa00e71e3d448570708cc87d47e5d12fdbc9a770
Valentyn Khalin <vkhalin@mirantis.com>
2017-02-17 18:55:54 +0200

    add travis ci failures

--- mk_verificator/tests/rabbit_mq/test_rabbit_cluster.py
+++ mk_verificator/tests/rabbit_mq/test_rabbit_cluster.py
@@ -5,10 +5,9 @@ from mk_verificator import utils
 
 
 @pytest.mark.parametrize(
-    ("group"),
+    "group",
     utils.get_groups(utils.get_configuration(__file__))
 )
-
 def test_checking_rabbitmq_cluster(local_salt_client, group):
     # request pillar data from rmq nodes
     rabbitmq_pillar_data = local_salt_client.cmd(
@@ -22,7 +21,8 @@ def test_checking_rabbitmq_cluster(local_salt_client, group):
     required_cluster_size_dict = {}
     for node in rabbitmq_pillar_data:
         data = json.loads(rabbitmq_pillar_data[node])
-        cluster_size_from_the_node = len(data['local']['rabbitmq:cluster']['members'])
+        cluster_size_from_the_node = \
+            len(data['local']['rabbitmq:cluster']['members'])
         required_cluster_size_dict.update({node: cluster_size_from_the_node})
 
     # request actual data from rmq nodes
@@ -36,15 +36,17 @@ def test_checking_rabbitmq_cluster(local_salt_client, group):
         list_of_nodes = 0
         for line in rabbit_actual_data[node].split('\n'):
             if 'running_nodes' in line:
-                list_of_nodes = re.findall(r'\'rabbit@(.+?)\'', line, re.IGNORECASE)
+                list_of_nodes = \
+                    re.findall(r'\'rabbit@(.+?)\'', line, re.IGNORECASE)
 
-        # update control dictionary with values {node:actual_cluster_size_for_node}
+        # update control dictionary with values
+        # {node:actual_cluster_size_for_node}
         if required_cluster_size_dict[node] != len(list_of_nodes):
-            control_dict.update({node:list_of_nodes})
+            control_dict.update({node: list_of_nodes})
 
-    assert not len(control_dict), \
-        '''Inconsistency found within cloud. RabbitMQ cluster
-              is probably broken, the cluster size for each node should be:
-              {} but the following nodes has other values: {}'''.format(
-                  len(required_cluster_size_dict.keys()),
-                  control_dict)
+    assert not len(control_dict), "Inconsistency found within cloud. " \
+                                  "RabbitMQ cluster is probably broken, " \
+                                  "the cluster size for each node " \
+                                  "should be: {} but the following " \
+                                  "nodes has other values: {}".format(
+        len(required_cluster_size_dict.keys()), control_dict)
