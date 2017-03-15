91d24e3e4383e501ec3f0f734b17eeb4fa197013
Oleksii Zhurba <legan4ik@gmail.com>
2017-03-15 17:39:04 +0000

    Reusing fixtures from tests folder

--- mk_verificator/benchmarks/scenarios/dd_tests/test_dd.py
+++ mk_verificator/benchmarks/scenarios/dd_tests/test_dd.py
@@ -1,44 +1,21 @@
 import pytest
-import random
 import time
-import mk_verificator.clients.nova as nova
 import mk_verificator.utils as utils
 import mk_verificator.utils.ssh_client as ssh
+import mk_verificator.fixtures.base
 # TODO
 # Create fixture for adding/removing keypair
 # Create fixture for checking security groups
-# Try to reuse fixtures from tests folder
-
-
-@pytest.yield_fixture(scope='module')
-def vm():
-    config = utils.get_configuration(__file__)
-    mk_nova = nova.Nova()
-    vm = mk_nova.create_vm(name='qa-framework-{}'.
-                           format(random.randint(1, 100)),
-                           key_name=config['key_name'])
-    mk_nova.wait_for_vm_status_is_active(vm.id)
-    yield vm
-    vm.delete()
-
-
-@pytest.yield_fixture(scope="module")
-def floating_ip():
-    mk_nova = nova.Nova()
-    floating_ip = mk_nova.client.floating_ips.create(
-                      mk_nova.client.floating_ip_pools.list()[0].name)
-    yield floating_ip
-    mk_nova.client.floating_ips.delete(floating_ip.id)
 
 
 @pytest.mark.parametrize(
     'count,block_size',
     [('4k', 262144), ('1M', 1024), ('1G', 1)]
 )
-def test_dd(vm, floating_ip, request, global_results,
+def test_dd(vm_kp, floating_ip, request, global_results,
             count, block_size):
     config = utils.get_configuration(__file__)
-    vm.add_floating_ip(floating_ip)
+    vm_kp.add_floating_ip(floating_ip)
     time.sleep(60)
     ssh_client = ssh.Node('vm', floating_ip.ip,
                           'ubuntu', pub_key=config['key_path'])
@@ -48,4 +25,4 @@ def test_dd(vm, floating_ip, request, global_results,
                                 format(count, block_size))
     global_results.update(request.node.name,
                           ssh_stdout.split(" ")[-2] +
-                          " "+ssh_stdout.split(" ")[-1])
+                          " " + ssh_stdout.split(" ")[-1])
