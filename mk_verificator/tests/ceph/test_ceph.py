2d09f45461bf2d10ff4e5e8a1a25bca743729a32
legan4ik <legan4ik@gmail.com>
2017-03-29 10:56:43 -0500

    Small corrections for ceph tests (#79)

---
+++ mk_verificator/tests/ceph/test_ceph.py
@@ -0,0 +1,11 @@
+from mk_verificator import utils
+
+
+def test_check_ceph_osd(local_salt_client):
+    config = utils.get_configuration(__file__)
+    osd_fail = \
+        local_salt_client.cmd(config["ceph_osd_probe_node"][0], 'cmd.run',
+                              ['ceph osd tree | grep down'])
+    assert not osd_fail.values()[0], \
+        "Some osds are in down state".format(
+        osd_fail.values()[0])
