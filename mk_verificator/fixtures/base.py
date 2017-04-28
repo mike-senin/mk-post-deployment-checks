40c8c4eccb7c8e6f724a0b750955bd231306bc45
Oleksii Zhurba <legan4ik@gmail.com>
2017-04-29 00:13:28 +0530

    Removing nova tests

--- mk_verificator/fixtures/base.py
+++ mk_verificator/fixtures/base.py
@@ -1,7 +1,5 @@
 import pytest
-import random
 import salt.client as client
-import mk_verificator.clients.nova as nova
 import mk_verificator.utils as utils
 import glanceclient.client as gl_client
 from keystoneauth1.identity import v3
@@ -24,35 +22,6 @@ def active_nodes(local_salt_client, skipped_nodes=None):
         if nodes[node_name] and node_name not in skipped_nodes
     ]
     return active_nodes
-
-
-@pytest.yield_fixture(scope="function")
-def vm():
-    mk_nova = nova.Nova()
-    vm = mk_nova.create_vm('qa-framework-{}'.format(random.randint(1, 100)))
-    mk_nova.wait_for_vm_status_is_active(vm.id)
-    yield vm
-    vm.delete()
-
-
-@pytest.yield_fixture(scope="function")
-def vm_kp():
-    config = utils.get_configuration(__file__)
-    mk_nova = nova.Nova()
-    vm = mk_nova.create_vm('qa-framework-{}'.format(random.randint(1, 100)),
-                           key_name=config['key_name'])
-    mk_nova.wait_for_vm_status_is_active(vm.id)
-    yield vm
-    vm.delete()
-
-
-@pytest.yield_fixture(scope="function")
-def floating_ip():
-    mk_nova = nova.Nova()
-    floating_pool = mk_nova.client.floating_ip_pools.list()[0].name
-    floating_ip = mk_nova.client.floating_ips.create(floating_pool)
-    yield floating_ip
-    mk_nova.client.floating_ips.delete(floating_ip.id)
 
 
 @pytest.fixture
