5d926cbb2897ece39c054b70399611bc28e3f892
dkulykovmirantis <den_0987654321@mail.ru>
2017-05-09 23:11:28 +0300

    Fix for galera test (#92)
    * Fix for galera test
    
    * Fix comments

--- mk_verificator/tests/test_galera_cluster.py
+++ mk_verificator/tests/test_galera_cluster.py
@@ -5,11 +5,12 @@ def test_galera_cluster_status(local_salt_client):
         ['salt-call mysql.status | grep -A1 wsrep_cluster_size | tail -n1'])
 
     size_cluster = []
+    amount = len(gs)
 
     for item in gs.values():
-        size_cluster.append(item.strip())
+        size_cluster.append(item.split('\n')[-1].strip())
 
-    assert size_cluster == ['3', '3', '3'], \
+    assert all(item == str(amount) for item in size_cluster), \
         '''There found inconsistency within cloud. MySQL galera cluster
               is probably broken, the cluster size gathered from nodes:
               {}'''.format(gs)
