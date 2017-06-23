b6d900ec4e38b8242105be5f341d5ff1fa4538ed
Oleksii Zhurba <ozhurba@mirantis.com>
2017-06-23 14:35:38 -0500

    Enhance test_repo_list

--- mk_verificator/tests/repo_list/test_repo_list.py
+++ mk_verificator/tests/repo_list/test_repo_list.py
@@ -1,5 +1,4 @@
 import pytest
-
 from mk_verificator import utils
 
 
@@ -14,21 +13,31 @@ def test_list_of_repo_on_nodes(local_salt_client, group):
     raw_actual_info = local_salt_client.cmd(
         group,
         'cmd.run',
-        ['cat /etc/apt/sources.list.d/*;cat /etc/apt/sources.list|grep deb'],
+        ['cat /etc/apt/sources.list.d/*;'
+         'cat /etc/apt/sources.list|grep deb|grep -v "#"'],
         expr_form='pcre')
-
     actual_repo_list = [item.replace('/ ', ' ')
                         for item in raw_actual_info.values()[0].split('\n')]
+    expected_salt_data = [repo['source'].replace('/ ', ' ')
+                          for repo in info_salt.values()[0]
+                          ['linux:system:repo'].values()]
 
-    expected_salt_data = [
-        repo['source'].replace('/ ', ' ')
-        for repo in info_salt.values()[0]['linux:system:repo'].values()
-    ]
+    diff = {}
+    my_set = set()
 
-    actual_repo_list.sort()
-    expected_salt_data.sort()
-
-    assert actual_repo_list == expected_salt_data, \
-        "There is difference between salt formula and " \
-        "local list of repo on node salt formula - {}" \
-        "local list - {}".format(expected_salt_data, actual_repo_list)
+    my_set.update(actual_repo_list)
+    my_set.update(expected_salt_data)
+    import json
+    for repo in my_set:
+        rows = []
+        if repo not in actual_repo_list:
+            rows.append("{}: {}".format("pillars", "+"))
+            rows.append("{}: No repo".format('config'))
+            diff[repo] = rows
+        elif repo not in expected_salt_data:
+            rows.append("{}: {}".format("config", "+"))
+            rows.append("{}: No repo".format('pillars'))
+            diff[repo] = rows
+    assert len(diff) <= 1, \
+        "Several problems found for {0} group: {1}".format(
+        group, json.dumps(diff, indent=4))
