160c24ca72d71c8e46960eb903931a48d5c57110
legan4ik <legan4ik@gmail.com>
2017-09-21 17:45:29 -0500

    Fixing hardcode in test_galera (#105)

--- mk_verificator/tests/test_galera_cluster.py
+++ mk_verificator/tests/test_galera_cluster.py
@@ -1,8 +1,9 @@
 def test_galera_cluster_status(local_salt_client):
     gs = local_salt_client.cmd(
-        'sql-*',
+        'I@galera:*',
         'cmd.run',
-        ['salt-call mysql.status | grep -A1 wsrep_cluster_size | tail -n1'])
+        ['salt-call mysql.status | grep -A1 wsrep_cluster_size | tail -n1'],
+        expr_form='pillar')
 
     size_cluster = []
     amount = len(gs)
